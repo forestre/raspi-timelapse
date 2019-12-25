934cec29dd5feb6d87d5096fd5d9d867b7336788
forestre <cloud.mn000@gmail.com>
2019-12-25 10:29:02 +0900

    fix

--- picamera_timelapse.py
+++ picamera_timelapse.py
@@ -5,15 +5,16 @@ from time import sleep
 
 case_name = '2019_1225'
 
-photo_interval = 600. # unit(sec)
-photo_time = 5. # unit(sec)
+photo_interval = 600.       # unit(sec)
+photo_time = 3600. * 24.    # unit(sec)
 
 photo_count = int(photo_time/photo_interval) + 1
 
 camera_width   = 1920
 camera_height  = 1080
 
-os.system('mkdir case_name')
+os.system('mkdirs ./case_name/')
+# os.system('mkdir case_name')
 
 with picamera.PiCamera() as camera:
 
@@ -23,5 +24,6 @@ with picamera.PiCamera() as camera:
 
     for i in range(photo_count):
         now = datetime.datetime.now()
-        camera.capture('image/' + case_name + '/image{0:04d}.jpg'.format(i))
+        # camera.capture('image/' + case_name + '/image{0:04d}.jpg'.format(i))
+        camera.capture(case_name + '/image{0:04d}.jpg'.format(i))
         sleep(photo_interval)
